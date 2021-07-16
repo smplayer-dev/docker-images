b8a516439cb96d3edf258722ce0521e12d349fe6
Ricardo <10128951+smplayer-dev@users.noreply.github.com>
2021-07-16 17:39:56 +0200

    Update run-smplayer.sh

--- run-smplayer.sh
+++ run-smplayer.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-docker run -e DISPLAY=$DISPLAY -v /tmp/.X11-unix/:/tmp/.X11-unix/ --name smplayer --volume=/run/user/1000/pulse:/run/user/1000/pulse -t smplayer
+docker run -e DISPLAY=$DISPLAY -v /tmp/.X11-unix/:/tmp/.X11-unix/ --name smplayer --volume=/run/user/1000/pulse:/run/user/1000/pulse -t wachii/smplayer:latest
