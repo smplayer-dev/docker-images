2d403570d15584a508f974a122eaafa3933ff47e
Ricardo <10128951+smplayer-dev@users.noreply.github.com>
2021-07-17 13:36:15 +0200

    Add the videos and music folders

--- run-smplayer.sh
+++ run-smplayer.sh
@@ -1,2 +1,7 @@
-#!/bin/sh
-docker run -e DISPLAY=$DISPLAY -v /tmp/.X11-unix/:/tmp/.X11-unix/ --name smplayer --volume=/run/user/1000/pulse:/run/user/1000/pulse -t wachii/smplayer:latest
+#!/bin/bash
+test -f ${XDG_CONFIG_HOME:-~/.config}/user-dirs.dirs && source ${XDG_CONFIG_HOME:-~/.config}/user-dirs.dirs
+docker run -e DISPLAY=$DISPLAY -v /tmp/.X11-unix/:/tmp/.X11-unix/ --name smplayer \
+           --volume=/run/user/1000/pulse:/run/user/1000/pulse \
+           --volume=$XDG_VIDEOS_DIR:/home/user/Videos \
+           --volume=$XDG_MUSIC_DIR:/home/user/Music \
+           -t wachii/smplayer:latest
